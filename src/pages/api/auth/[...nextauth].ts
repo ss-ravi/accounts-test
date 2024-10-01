9a4f4d3cd70e807e7b7fa5efcb1b6cfeee4ea768
Ravi <ss-ravi@users.noreply.github.com>
2024-10-01 12:17:58 +0900

    Refactor Auth0 provider configuration

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,18 @@
-import NextAuth from 'next-auth';
-import Auth0Provider from 'next-auth/providers/auth0';
+import NextAuth from "next-auth";
+import Auth0Provider from "next-auth/providers/auth0";
+
+const clientId = process.env.AUTH0_CLIENT_ID!;
+const clientSecret = process.env.AUTH0_CLIENT_SECRET!;
+const issuer = process.env.AUTH0_ENDPOINT!;
 
 export default NextAuth({
   providers: [
     Auth0Provider({
-      name: 'my auth0 provider',
-      id: 'my-auth0',
-      clientId: process.env.AUTH0_CLIENT_ID!,
-      clientSecret: process.env.AUTH0_CLIENT_SECRET!,
-      issuer: process.env.AUTH0_ENDPOINT!,
+      name: "my auth0 provider",
+      id: "my-auth0",
+      clientId,
+      clientSecret,
+      issuer,
       token: {
         params: {
           audience: `${process.env.AUTH0_ENDPOINT!}/api/v2/`,
