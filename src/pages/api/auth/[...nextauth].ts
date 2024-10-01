4ee13815635e2e4b54dca5a26dba2a42f11837a1
Ravi <ss-ravi@users.noreply.github.com>
2024-10-01 12:18:31 +0900

    set redirectUrl in provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,6 +5,8 @@ const clientId = process.env.AUTH0_CLIENT_ID!;
 const clientSecret = process.env.AUTH0_CLIENT_SECRET!;
 const issuer = process.env.AUTH0_ENDPOINT!;
 
+const redirectUri = `${process.env.NEXTAUTH_URL!}/callback/my-auth0`;
+
 export default NextAuth({
   providers: [
     Auth0Provider({
@@ -14,16 +16,29 @@ export default NextAuth({
       clientSecret,
       issuer,
       token: {
+        request: async ({ client, params, checks }) => {
+          return {
+            tokens: await client.callback(redirectUri, params, checks),
+          };
+        },
         params: {
+          redirectUri,
           audience: `${process.env.AUTH0_ENDPOINT!}/api/v2/`,
         },
       },
       authorization: {
         params: {
+          redirectUri,
           audience: `${process.env.AUTH0_ENDPOINT!}/api/v2/`,
         },
       },
       idToken: true,
     }),
   ],
+  callbacks: {
+    redirect: ({ url }) => {
+      console.log(url);
+      return url;
+    },
+  },
 });
